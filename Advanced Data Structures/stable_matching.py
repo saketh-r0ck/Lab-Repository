04423b1e2b6d2a614be1a5ec919c6f0702e3d068
saketh-r0ck <saisaketh5022@gmail.com>
2024-09-03 23:51:43 +0530

    gale-shapley added

--- Advanced Data Structures/stable_matching.py
+++ Advanced Data Structures/stable_matching.py
@@ -5,7 +5,8 @@ class Gale_Shapely:
 		self.free_man = list(proposing.keys())
 		self.man = proposing
 		self.woman = proposed
-			
+		
+		
 	
 	def stable_matching(self,men_preference,women_preference):
 		while len(self.free_man) >0:
@@ -13,31 +14,44 @@ class Gale_Shapely:
 			
 			m_list = men_preference[self.man[m]]
 			for w in m_list:
-				engaged = self.woman_match[self.woman[w]]
+				women_idx = self.woman[w]
+
+				engaged = self.woman_match[women_idx]
+				print(str(m) + " "+ str(engaged))
 				if engaged is None:
 					self.men_match[self.man[m]] = w
-					self.women_match[self.woman[w]] = m
+					
+					self.woman_match[self.woman[w]] = m
+					
+					break
+				else: 
+					w_list = women_preference[women_idx]
+					if w_list.index(m) < w_list.index(engaged):
+						self.men_match[self.man[engaged]] = None
+						self.free_man.append(engaged)
+						self.men_match[self.man[m]] = w
+						self.woman_match[self.woman[w]] = m
+						break
 		print(self.men_match)
-		print(self.womam_match)
+		print(self.woman_match)
 
 
 if __name__ == "__main__":
 	
-	# student_pre = [['d','a','b','c'],
-	# 		['c','b','a','d'],
-	# 		['c','b','a','d'],
-	# 		['d','a','b','c']]
+	student_pre = [['d','a','b','c'],
+			['c','b','a','d'],
+			['c','b','a','d'],
+			['d','a','b','c']]
 			
-	# university_pre = [['C','D','B','A'],
-	# 		['D','C','A','B'],
-	# 		['A','C','B','D'],
-	# 		['B','D','A','C']]
-	# student = {'A':0,'B':1,'C':2,'D':3}
-	# university = {'a':0,'b':1,'c':2,'d':3}
+	university_pre = [['C','D','B','A'],
+			['D','C','A','B'],
+			['A','C','B','D'],
+			['B','D','A','C']]
+	student = {'A':0,'B':1,'C':2,'D':3}
+	university = {'a':0,'b':1,'c':2,'d':3}
 	
-	student_pre = [['A','B','C'],['B','A','C'],['A','B','C']]
-	university_pre = [['Y','X','Z'],['X','Y','Z'],['X','Y','Z']]
-	student = {'A':0,'B':1,'C':2}
-	university = {'X':0,'Y':1,'Z':2}
 	obj = Gale_Shapely(student,university)
 	obj.stable_matching(student_pre,university_pre)
+
+	obj2 = Gale_Shapely(university,student)
+	obj2.stable_matching(university_pre,student_pre)
