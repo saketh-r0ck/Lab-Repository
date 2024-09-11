4c4bf560f3cfa2dcaefaa5706679252a37b0a21e
saketh-it021 <saisaketh0695@gmail.com>
2024-09-11 20:28:18 +0530

    polynomial completed

--- Advanced Data Structures/reccursive_polynomial_multiplication.py
+++ Advanced Data Structures/reccursive_polynomial_multiplication.py
@@ -1,31 +1,53 @@
 from math import sin,cos,pi
+import numpy as np
 def FFT(a,inverse_flag):
-    n = len(a)
-    if n==1 :
-        return a
-    a_e ,a_o = a[::2] , a[1::2]
-    y_e , y_o = FFT(a_e,0) , FFT(a_o,0)
+	n = len(a)
+	if n==1 :
+		return a
+	a_e ,a_o = a[::2] , a[1::2]
+	y_e , y_o = FFT(a_e,inverse_flag) , FFT(a_o,inverse_flag)
 
-    if inverse_flag:
-        theta = -2*pi/n 
-    else:
-        theta = 2*pi/n
+	if inverse_flag:	
+		theta = -2*np.pi/n 
+	else:
+		theta = 2*np.pi/n
 
-    w = list(complex(cos(theta*i), sin(theta*i)) for i in range(n)) 
-    y = [0]*n
-    half = n//2
-    for k in range(half):
-        y[k] = y_e[k] + w[k] * y_o[k]
-        y[k + half] = y_e[k] - w[k] * y_o[k]
+	w = 1
+	wn = np.exp(1j * theta) 
+	y = [0]*n
 
-    if inverse_flag: 
-        return [val/n for val in y]
-    else:    
-        return y
+	for k in range(n//2):
+		y[k] = y_e[k] + w * y_o[k]
+		y[k + n//2] = y_e[k] - w * y_o[k]
+		if inverse_flag:
+			y[k] /= 2
+			y[k + n//2 ] /= 2
+		w = w * wn
+	return y
+        
+def polynomial_multiplication(a1,a2):
+	n = 1
+	while n < len(a1)+len(a2)-1:
+		n*=2
+	
+	a1.extend([0]*(n - len(a1)))
+	a2.extend([0]*(n - len(a2)))
+	
+	b1 = FFT(a1,0)
+	b2 = FFT(a2,0)
+	
+	point_mul = np.multiply(b1,b2)
+	
+	result = FFT(point_mul,1)
+	result = [round(x.real) for x in result]
+	while len(result) > 1 and result[-1] == 0:
+        	result.pop()
+	return result
+	
 
 if __name__ == '__main__':
-    a = [1,2,3,4]
-    b = FFT(a,0)
-    c = FFT(b,1)
-    print(c)
-    print(b)
+    a1 = [9,-10,7,6]
+    a2 = [-5,4,0,-2]
+    print(polynomial_multiplication(a1,a2))
+    
+    
