e3870137bb8d18f6ce1a4b89e570555a7624c705
saketh-it021 <saisaketh0695@gmail.com>
2024-09-18 17:30:42 +0530

    weighted interval Scheduling done

--- Advanced Data Structures/weighted_interval_scheduling.py
+++ Advanced Data Structures/weighted_interval_scheduling.py
@@ -1,28 +1,49 @@
 
-def find_p(jobs,finish):
-	large_index = 0
-	end = jobs[finish]
-	for i in jobs:
-		print(large_index)
-		if i == end:
-			return large_index
-		if i[1] <= end[0]:
-			large_index = max(large_index,i[0])
+def find_p(jobs,end_idx):
+	end = jobs[end_idx]
+	#cnt = 0		
+	#for i in jobs:
+	#	
+	#	if i == end:
+	#		return cnt
+	#	if i[1] <= end[0]:
+	#		cnt +=1
+			
+	low = 0
+	high = end_idx
+	while low <= high:
+		mid = (low + high) // 2
 		
+		if jobs[mid][1] <= jobs[end_idx][0]:
+			if jobs[mid+1][1] <= jobs[end_idx][0]:
+				low = mid + 1
+			else: 
+				return mid + 1
+		else:
+			high = mid - 1 
+	return 0
 
 
+
+	
 def find_max_wight(jobs):
-	print(jobs)
+
 	jobs.sort(key=lambda x:x[1])
 	print(jobs)
 	
 	p = [0]*len(jobs)
-	#for i in range(len(jobs)):
-	p[3] = find_p(jobs,3)
+	for i in range(len(jobs)):
+		p[i] = find_p(jobs,i)
 	print(p)
-
+	
+	opt = [0]*(len(jobs)+1)
+	for j in range(1,len(jobs)+1):
+		opt[j] = max(jobs[j-1][2] + opt[p[j-1]] , opt[j-1])
+	
+	return opt
 
 
 if __name__ == '__main__':
     jobs = [(0,3,3),(1,4,2),(3,6,1),(0,5,4),(4,7,2),(3,9,5),(5,10,2),(8,10,1)]
-    find_max_wight(jobs)
+    optimal = find_max_wight(jobs)
+    print(optimal)
